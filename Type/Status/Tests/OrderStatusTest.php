5dee371a93944ecd320de50d563d1c3baa25affd
kingstar <admin@baks.dev>
2023-10-18 15:08:58 +0300

    Baks Development

---
+++ Type/Status/Tests/OrderStatusTest.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Orders\Order\Type\Status\Tests;
+
+use BaksDev\Orders\Order\Type\Status\OrderStatus;
+use BaksDev\Orders\Order\Type\Status\OrderStatus\Collection\OrderStatusCollection;
+use BaksDev\Orders\Order\Type\Status\OrderStatusType;
+use BaksDev\Wildberries\Orders\Type\WildberriesStatus\Status\Collection\WildberriesStatusInterface;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\Attribute\When;
+
+/**
+ * @group orders-order
+ */
+#[When(env: 'test')]
+final class OrderStatusTest extends KernelTestCase
+{
+    public function testUseCase(): void
+    {
+        /** @var OrderStatusCollection $OrderStatusCollection */
+        $OrderStatusCollection = self::getContainer()->get(OrderStatusCollection::class);
+
+        /** @var WildberriesStatusInterface $case */
+        foreach($OrderStatusCollection->cases() as $case)
+        {
+            $OrderStatus = new OrderStatus($case->getValue());
+
+            self::assertTrue($OrderStatus->equals($case::class)); // немспейс интерфейса
+            self::assertTrue($OrderStatus->equals($case)); // объект интерфейса
+            self::assertTrue($OrderStatus->equals($case->getValue())); // срока
+            self::assertTrue($OrderStatus->equals($OrderStatus)); // объект класса
+
+            $OrderStatusType = new OrderStatusType();
+            $platform = $this->getMockForAbstractClass(AbstractPlatform::class);
+
+            $convertToDatabase = $OrderStatusType->convertToDatabaseValue($OrderStatus, $platform);
+            self::assertEquals($OrderStatus->getOrderStatusValue(), $convertToDatabase);
+
+            $convertToPHP = $OrderStatusType->convertToPHPValue($convertToDatabase, $platform);
+            self::assertInstanceOf(OrderStatus::class, $convertToPHP);
+            self::assertEquals($case, $convertToPHP->getOrderStatus());
+
+        }
+
+    }
+}

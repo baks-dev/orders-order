b6854da641c72247d96b775fd8fcb07fa157f70d
kingstar <admin@baks.dev>
2024-05-14 15:33:02 +0300

    Baks Development

---
+++ Repository/CurrentOrderNumber/CurrentOrderNumberRepository.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Orders\Order\Repository\CurrentOrderNumber;
+
+use BaksDev\Core\Doctrine\ORMQueryBuilder;
+use BaksDev\Orders\Order\Entity\Event\OrderEvent;
+use BaksDev\Orders\Order\Entity\Order;
+use Doctrine\DBAL\Types\Types;
+
+final class CurrentOrderNumberRepository implements CurrentOrderNumberInterface
+{
+    private ORMQueryBuilder $ORMQueryBuilder;
+
+    public function __construct(ORMQueryBuilder $ORMQueryBuilder)
+    {
+        $this->ORMQueryBuilder = $ORMQueryBuilder;
+    }
+
+    /**
+     * Метод возвращает текущее активное событие заказа
+     */
+    public function getCurrentOrderEvent(int|string $number): ?OrderEvent
+    {
+        $orm = $this->ORMQueryBuilder->createQueryBuilder(self::class);
+
+        $orm
+            ->from(Order::class, 'orders')
+            ->where('orders.number = :order')
+            ->setParameter('order', (string) $number, Types::STRING);
+
+        $orm
+            ->select('event')
+            ->join(
+                OrderEvent::class,
+                'event',
+                'WITH',
+                'event.id = orders.event'
+            );
+
+        return $orm->getOneOrNullResult();
+    }
+
+}
---
+++ Repository/ExistsOrderNumber/ExistsOrderNumberRepository.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Orders\Order\Repository\ExistsOrderNumber;
+
+use BaksDev\Core\Doctrine\DBALQueryBuilder;
+use BaksDev\Orders\Order\Entity\Order;
+use Doctrine\DBAL\ParameterType;
+
+
+final class ExistsOrderNumberRepository implements ExistsOrderNumberInterface
+{
+    private DBALQueryBuilder $DBALQueryBuilder;
+
+    public function __construct(DBALQueryBuilder $DBALQueryBuilder)
+    {
+        $this->DBALQueryBuilder = $DBALQueryBuilder;
+    }
+
+    /**
+     * Метод проверяет, имеется ли заказа с указанным номером
+     */
+    public function isExists(int|string $number): bool
+    {
+        $dbal = $this->DBALQueryBuilder->createQueryBuilder(self::class);
+
+        $dbal
+            ->from(Order::class, 'ord')
+            ->where('ord.number = :number')
+            ->setParameter('number', (string) $number, ParameterType::STRING);
+
+        return $dbal->fetchExist();
+    }
+}

ca958e5dbde018da407b3547cdcaeb8e24a96bfa
kingstar <admin@baks.dev>
2025-01-07 23:28:13 +0300

    Baks Development

--- UseCase/Admin/New/User/OrderUserForm.php
+++ UseCase/Admin/New/User/OrderUserForm.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -59,10 +59,6 @@ final class OrderUserForm extends AbstractType
                 /** @var OrderUserDTO $data */
                 $data = $event->getData();
                 $form = $event->getForm();
-
-
-                //                dump('PRE_SET_DATA '.$this::class);
-                //                dump($data->getUserProfile()?->getType());
 
 
                 if($data->getUserProfile()?->getType())

7652fe9a9b6a2bdf99381194d578c351075a0bf7
apelipenko <riichikunda@gmail.com>
2025-04-22 17:20:20 +0300

    reports

---
+++ UseCase/Admin/Status/Tests/OrderStatusCompleteTest.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * Copyright 2025.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Orders\Order\UseCase\Admin\Status\Tests;
+
+use BaksDev\Orders\Order\Entity\Order;
+use BaksDev\Orders\Order\Type\Event\OrderEventUid;
+use BaksDev\Orders\Order\Type\Status\OrderStatus;
+use BaksDev\Orders\Order\UseCase\Admin\Status\OrderStatusDTO;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\Attribute\When;
+use BaksDev\Orders\Order\UseCase\Admin\Status\OrderStatusHandler;
+
+/**
+ * @group orders-order
+ * @group orders-order-repository
+ * @depends BaksDev\Orders\Order\UseCase\Admin\Edit\Tests\OrderNewTest::class
+ */
+#[When(env: 'test')]
+final class OrderStatusCompleteTest extends KernelTestCase
+{
+    public static function testUseCase(): void
+    {
+        $orderStatusDTO = new OrderStatusDTO(OrderStatus\Collection\OrderStatusCompleted::STATUS, new OrderEventUid());
+
+        /** @var OrderStatusHandler $statusHandler */
+        $statusHandler = self::getContainer()->get(OrderStatusHandler::class);
+        $handle = $statusHandler->handle($orderStatusDTO);
+
+        self::assertTrue(($handle instanceof Order), $handle.': Ошибка OrderStatus');
+    }
+}

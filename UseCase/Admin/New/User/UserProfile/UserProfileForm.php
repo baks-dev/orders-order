a7c5abf555b2f4219ad108b575832d7309f8e61b
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-17 17:27:00 +0300

    Baks Development
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- UseCase/Admin/New/User/UserProfile/UserProfileForm.php
+++ UseCase/Admin/New/User/UserProfile/UserProfileForm.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -140,6 +140,11 @@ final class UserProfileForm extends AbstractType
                     $data->setValue(new ArrayCollection());
 
                     $fields = $this->fieldValue->get($data->getType());
+
+                    if(empty($fields))
+                    {
+                        return;
+                    }
 
                     foreach($fields as $key => $field)
                     {

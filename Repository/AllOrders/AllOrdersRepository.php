8557146b97309b3d17cc0a89750bbccdaad94a3c
MichelAngelo <baksdevelopmen@gmail.com>
2025-07-31 11:50:29 +0300

    Baks Development
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Repository/AllOrders/AllOrdersRepository.php
+++ Repository/AllOrders/AllOrdersRepository.php
@@ -138,7 +138,7 @@ final class AllOrdersRepository implements AllOrdersInterface
                 '
                     order_invariable.main = orders.id AND 
                     (order_invariable.usr IS NULL OR order_invariable.usr = :user)
-                '.($this->status?->equals(OrderStatusNew::class) ? ' (order_invariable.profile IS NULL OR order_invariable.profile = :profile)' : ' order_invariable.profile = :profile')
+                '.($this->status?->equals(OrderStatusNew::class) ? ' AND (order_invariable.profile IS NULL OR order_invariable.profile = :profile)' : ' order_invariable.profile = :profile')
             )
             ->setParameter(
                 key: 'user',

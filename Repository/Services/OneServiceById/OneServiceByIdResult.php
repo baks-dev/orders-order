24a39e613d77751969798989d3b318abc40e2277
MichelAngelo <baksdevelopmen@gmail.com>
2026-02-13 00:49:00 +0300

    Baks Development
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Repository/Services/OneServiceById/OneServiceByIdResult.php
+++ Repository/Services/OneServiceById/OneServiceByIdResult.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -72,12 +72,12 @@ final readonly class OneServiceByIdResult
         return $this->preview;
     }
 
-    public function getFrom(): string
+    public function getFrom(): DateTimeImmutable
     {
         return new DateTimeImmutable($this->frm);
     }
 
-    public function getTo(): string
+    public function getTo(): DateTimeImmutable
     {
         return new DateTimeImmutable($this->upto);
     }

7c6f9bb9fba742e9cad1db03639d4eb3b1dcb020
adminap <shestopalov92@gmail.com>
2026-02-19 15:34:29 +0300

    fork: PR-19.02.2026. CTE для запроса AllOrdersRepository

--- Repository/AllOrders/AllOrdersResult.php
+++ Repository/AllOrders/AllOrdersResult.php
@@ -19,6 +19,7 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
+ *
  */
 
 declare(strict_types=1);
@@ -117,7 +118,12 @@ final  class AllOrdersResult
 
     public function getOrderNumber(): string
     {
-        return $this->order_posting ?? 'Не указан';
+        return $this->order_number;
+    }
+
+    public function getOrderPosting(): ?string
+    {
+        return $this->order_posting;
     }
 
     public function getOrderCreated(): DateTimeImmutable

fd5556640cc1683890f19031df9520700a00c92a
Dmitrii <ladovod@gmail.com.com>
2026-05-13 20:46:32 +0500

    Правит bindProjectProfile на isProjectProfile для сезонности

--- Repository/AllOrdersReport/AllOrdersReportRepository.php
+++ Repository/AllOrdersReport/AllOrdersReportRepository.php
@@ -321,7 +321,7 @@ final class AllOrdersReportRepository implements AllOrdersReportInterface
                 'product_project',
                 '
                     product_project.product = product_event.main
-                    '.(true === $dbal->bindProjectProfile()
+                    '.(true === $dbal->isProjectProfile()
                     ? 'AND product_project.profile = :'.$dbal::PROJECT_PROFILE_KEY
                     : 'AND product_project.profile IS NULL'),
             );
